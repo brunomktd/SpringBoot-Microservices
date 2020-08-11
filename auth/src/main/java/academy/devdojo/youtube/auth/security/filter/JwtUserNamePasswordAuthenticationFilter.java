8670d1d80c4b0557b3da3a197b54632808a52653
Bruno Oliveira <bruno.mktd@gmail.com>
2020-08-11 10:53:47 -0300

    Spring Boot application Microservices

---
+++ auth/src/main/java/academy/devdojo/youtube/auth/security/filter/JwtUserNamePasswordAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package academy.devdojo.youtube.auth.security.filter;
+
+import academy.devdojo.youtube.core.model.ApplicationUser;
+import academy.devdojo.youtube.core.property.JwtConfiguration;
+import academy.devdojo.youtube.security.token.creator.TokenCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static java.util.Collections.emptyList;
+
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class JwtUserNamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtConfiguration jwtConfiguration;
+    private final TokenCreator tokenCreator;
+
+    @Override
+    @SneakyThrows
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        log.info("Tentativa de autenticação...");
+        ApplicationUser applicationUser = new ObjectMapper().readValue(request.getInputStream(), ApplicationUser.class);
+
+        if(applicationUser == null){
+            throw new UsernameNotFoundException("Não foi possível recuperar o nome de usuário ou senha");
+        }
+
+        log.info("Criação do objeto de autenticação para o usuário '{}' e chamada de UserDetailServiceImpl loadByUsername", applicationUser.getUsername());
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(applicationUser.getUsername(), applicationUser.getPassword(), emptyList());
+
+        usernamePasswordAuthenticationToken.setDetails(applicationUser);
+
+        return authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    @Override
+    @SneakyThrows
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) {
+        log.info("A autenticação foi bem-sucedida para o usuário '{}', gerando token JWE", auth.getName());
+
+        SignedJWT signedJWT = tokenCreator.createSignedJWT(auth);
+
+        String encryptedToken = tokenCreator.encryptToken(signedJWT);
+
+        log.info("Token gerado com sucesso, adicionando-o ao cabeçalho de resposta");
+
+        // habilitando o javascript para pegar os dois valores
+        response.addHeader("Access-Control-Expose-Headers", "XSRF-TOKEN, " + jwtConfiguration.getHeader().getName());
+
+        response.addHeader(jwtConfiguration.getHeader().getName(), jwtConfiguration.getHeader().getPrefix() + encryptedToken);
+
+    }
+
+
+
+}
